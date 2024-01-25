3b224f3d601301810122809bbf960f73ec5a7085
dmitrij <dmitrijkrymskij6@gmail.com>
2024-01-26 01:01:16 +0400

    feat: add task create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
-const create = async () => {
-    // Write your code here 
-};
+import { promises as fsPromises } from 'fs';
 
-await create();
+const create = async () => {
+    try {
+      await fsPromises.access('src/fs/files/fresh.txt');
+      throw new Error('FS operation failed');
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        await fsPromises.writeFile('src/fs/files/fresh.txt', 'I am fresh and young', 'utf8');
+        console.log('File created successfully');
+      } else {
+        console.error(err);
+      }
+    }
+  };
+  
+  await create();
+  
