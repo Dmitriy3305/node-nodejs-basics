7e6f15ed33a8495e5fdd26cd63f8f5f436bb0489
dmitrij <dmitrijkrymskij6@gmail.com>
2024-01-27 21:00:06 +0400

    feat: add task list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import { promises as fsPromises } from "fs";
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const isExistSource = await fsPromises
+          .access("src/fs/files")
+          .then(() => true)
+          .catch(() => false);
+    
+        if (!isExistSource) {
+          throw new Error("FS operation failed");
+        }
+    
+        const files = await fsPromises.readdir("src/fs/files");
+        for (const file of files) {
+         console.log(file);
+        }
+      } catch (error) {
+        console.error(error);
+      }
 };
 
 await list();
