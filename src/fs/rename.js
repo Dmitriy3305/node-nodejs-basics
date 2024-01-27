695c622e59dd723c0e3921b51ac914452d784269
dmitrij <dmitrijkrymskij6@gmail.com>
2024-01-27 15:25:18 +0400

    feat: add task rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import { promises as fsPromises } from "fs";
+
 const rename = async () => {
-    // Write your code here 
+  const isExistSource = await fsPromises
+    .access("src/fs/files/properFilename.md")
+    .then(() => true)
+    .catch(() => false);
+
+  const isExistTarget = await fsPromises
+    .access("src/fs/files/wrongFilename.txt")
+    .then(() => true)
+    .catch(() => false);
+
+  if (isExistSource || !isExistTarget) {
+    throw new Error("FS operation failed");
+  }
+
+  await fsPromises.rename(
+    "src/fs/files/wrongFilename.txt",
+    "src/fs/files/properFilename.md"
+  );
 };
 
 await rename();
