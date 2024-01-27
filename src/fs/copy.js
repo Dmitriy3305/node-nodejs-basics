5682009453933119c4504f1f20930a3998be150e
dmitrij <dmitrijkrymskij6@gmail.com>
2024-01-27 14:04:33 +0400

    feat: add task copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import { promises as fsPromises } from "fs";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const isExistSource = await fsPromises
+      .access("src/fs/files")
+      .then(() => true)
+      .catch(() => false);
+
+    const isExistTarget = await fsPromises
+      .access("src/fs/files_copy")
+      .then(() => true)
+      .catch(() => false);
+
+    if (isExistTarget || !isExistSource) {
+      throw new Error("FS operation failed");
+    }
+
+    await fsPromises.mkdir("src/fs/files_copy");
+    const files = await fsPromises.readdir("src/fs/files");
+    for (const file of files) {
+      await fsPromises.copyFile(`src/fs/files/${file}`, `src/fs/files_copy/${file}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await copy();
