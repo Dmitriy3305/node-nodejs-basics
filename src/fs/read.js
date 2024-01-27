16fec4f93453d724f79f17b2004f775f9ec68d8e
dmitrij <dmitrijkrymskij6@gmail.com>
2024-01-27 21:07:26 +0400

    feat: add task read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { promises as fsPromises } from "fs";
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const isExistSource = await fsPromises
+          .access("src/fs/files/fileToRead.txt")
+          .then(() => true)
+          .catch(() => false);
+    
+        if (!isExistSource) {
+          throw new Error("FS operation failed");
+        }
+    
+        const fileContent = await fsPromises.readFile("src/fs/files/fileToRead.txt", 'utf-8');
+        console.log(fileContent)
+      } catch (error) {
+        console.error(error);
+      } 
 };
 
 await read();
