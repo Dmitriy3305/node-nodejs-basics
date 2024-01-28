0186ebb971c85e8676df0af70f57be6f84c8ded4
dmitrij <dmitrijkrymskij6@gmail.com>
2024-01-28 14:51:15 +0400

    feat: add task read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { createReadStream } from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream("src/streams/files/fileToRead.txt");
+
+  stream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  stream.on("error", (error) => {
+    console.error("Error:", error);
+  });
+
+  return new Promise((res, rej) => {
+    stream.on("end", res);
+    stream.on("error", rej);
+  });
 };
 
 await read();
