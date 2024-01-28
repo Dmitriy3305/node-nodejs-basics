01cf4423ec431b8c655e6a46d3288a830c2a090c
dmitrij <dmitrijkrymskij6@gmail.com>
2024-01-28 14:35:37 +0400

    feat: add task calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  return new Promise((resolve, reject) => {
+    const stream = createReadStream(
+      "src/hash/files/fileToCalculateHashFor.txt"
+    );
+    const hash = createHash("sha256");
+
+    stream.on("data", (chunk) => {
+      hash.update(chunk);
+    });
+
+    stream.on("end", () => {
+      const hashHex = hash.digest("hex");
+      console.log(`SHA256 hash of the file: ${hashHex}`);
+      resolve(hashHex);
+    });
+
+    stream.on("error", (error) => {
+      console.error("Error while calculating the hash:", error);
+      reject(error);
+    });
+  });
 };
 
 await calculateHash();
