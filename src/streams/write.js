c7531c378309aeb6c1c3fefc5e56f869a00cb3d7
dmitrij <dmitrijkrymskij6@gmail.com>
2024-01-28 18:42:14 +0400

    feat: add task write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+
 const write = async () => {
-    // Write your code here 
+  const writeStream = fs.createWriteStream("src/streams/files/fileToWrite.txt");
+
+  const finished = new Promise((res, rej) => {
+    writeStream.on("finish", res);
+    writeStream.on("error", rej);
+  });
+
+  process.stdin.pipe(writeStream);
+
+  await finished;
+  console.log("Data has been written to fileToWrite.txt");
 };
 
 await write();
