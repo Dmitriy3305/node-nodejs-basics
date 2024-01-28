3346825c179fc78291424def4e0869ce7e4fb42a
dmitrij <dmitrijkrymskij6@gmail.com>
2024-01-28 19:11:41 +0400

    feat: add tasks compress.js and decompress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import zlib from "zlib";
+import { pipeline } from "stream";
+import { promisify } from "util";
+
+const pipelineAsync = promisify(pipeline);
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = zlib.createGzip();
+  const source = fs.createReadStream("src/zip/files/fileToCompress.txt");
+  const destination = fs.createWriteStream("src/zip/files/archive.gz");
+
+  await pipelineAsync(source, gzip, destination);
+
+  await fs.promises.unlink("src/zip/files/fileToCompress.txt");
+
+  console.log("fileToCompress.txt successfully compressed to archive.gz");
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import zlib from "zlib";
+import { pipeline } from "stream";
+import { promisify } from "util";
+
+const pipelineAsync = promisify(pipeline);
+
 const decompress = async () => {
-    // Write your code here 
+  const gunzip = zlib.createGunzip();
+  const source = fs.createReadStream("src/zip/files/archive.gz");
+  const destination = fs.createWriteStream("src/zip/files/fileToCompress.txt");
+
+  await pipelineAsync(source, gunzip, destination);
+
+  await fs.promises.unlink("src/zip/files/archive.gz");
+
+  console.log("archive.gz successfully decompressed to fileToCompress.txt");
 };
 
 await decompress();
