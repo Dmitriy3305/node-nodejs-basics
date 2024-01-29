a9274028bfada3948165c7cd641c0eadf60c4c32
dmitrij <dmitrijkrymskij6@gmail.com>
2024-01-29 22:14:35 +0400

    feat: add task worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import os from "os";
+import { Worker } from "worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  let arrPromises = [];
+  const numCores = os.cpus().length;
+
+  for (let i = 0; i < numCores; i++) {
+    let worker = new Worker(new URL("./worker.js", import.meta.url));
+    let n = 10 + i;
+    let promise = new Promise((res) => {
+      worker.once("error", (error) => res({ status: error.message }));
+      worker.once("message", (result) => res(result));
+      worker.postMessage(n);
+    });
+    arrPromises.push(promise);
+  }
+
+  const arrResults = await Promise.all(arrPromises);
+  console.log(arrResults);
 };
 
 await performCalculations();
